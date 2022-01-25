0ccf656ea8fc947513807a8dc8923f5d46f106ce
Gene Boggs <gene@ology.net>
2022-01-25 12:17:41 -0800

    Bump version

--- lib/MIDI/Bassline/Walk.pm
+++ lib/MIDI/Bassline/Walk.pm
@@ -2,7 +2,7 @@ package MIDI::Bassline::Walk;
 
 # ABSTRACT: Generate walking basslines
 
-our $VERSION = '0.0304';
+our $VERSION = '0.0305';
 
 use Data::Dumper::Compact qw(ddc);
 use Carp qw(croak);
