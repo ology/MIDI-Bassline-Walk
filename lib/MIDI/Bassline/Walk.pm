78a291ab498df97c832cedd96e3fb3efae6c7a0f
Gene Boggs <gene@ology.net>
2022-07-01 23:06:35 -0700

    Update doc

--- lib/MIDI/Bassline/Walk.pm
+++ lib/MIDI/Bassline/Walk.pm
@@ -45,7 +45,7 @@ a drop-in bass player.  Import rendered MIDI into a DAW and alter
 notes until they sound suitable.
 
 The "formula" implemented by this module is basically: "Play any notes
-of the chord, or chord-root scale."
+of the chord, modal chord scale, or chord-root scale."
 
 The chords recognized by this module, are those known to
 L<Music::Chord::Note>.  Please see the source of that module for the
